2b43ff6bca04eb441da1bd8a3b9f9c38264166b3
Ashwani Kumar Vishwakarma <ashwanikumar3811@gmail.com>
2023-11-12 15:39:15 +0530

    work on sidebar

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import Image from "next/image";
+
+const Logo = () => {
+  return <Image width={130} height={130} alt='logo' src={"/logo.svg"} />;
+};
+
+export default Logo;
---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import SidebarItem from "./sidebar-item";
+
+import { Compass, Layout } from "lucide-react";
+
+const guestRoutes = [
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: Compass,
+    label: "Browse",
+    href: "/search",
+  },
+];
+
+const SidebarRoutes = () => {
+  const routes = [...guestRoutes];
+
+  return (
+    <div className='flex flex-col w-full'>
+      {routes.map((route) => (
+        <SidebarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SidebarRoutes;

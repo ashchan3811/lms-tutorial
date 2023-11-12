b491273d78e0f0a28750b5f879549b19274280cc
Ashwani Kumar Vishwakarma <ashwanikumar3811@gmail.com>
2023-11-12 15:47:47 +0530

    added narbar

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,12 @@
+import MobileSidebar from "./mobile-sidebar";
+
+
+const Navbar = () => {
+  return (
+    <div className='p-4 border-b h-full flex items-center bg-white shadow-sm'>
+      <MobileSidebar />
+    </div>
+  );
+};
+
+export default Navbar;

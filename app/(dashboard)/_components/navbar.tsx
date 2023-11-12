33b68a93b88eb4c0e20d2fef6c90c0b0b27258ef
Ashwani Kumar Vishwakarma <ashwanikumar3811@gmail.com>
2023-11-12 16:09:31 +0530

    fix formatting

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,6 +1,5 @@
-import NavbarRoutes from '@/components/navbar-routes';
+import NavbarRoutes from "@/components/navbar-routes";
 import MobileSidebar from "./mobile-sidebar";
-
 
 const Navbar = () => {
   return (

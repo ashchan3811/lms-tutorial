295bf85c26dcb9d77bb91b9e71d77fb27ac5efd2
Ashwani Kumar Vishwakarma <ashwanikumar3811@gmail.com>
2023-11-18 22:07:37 +0530

    added animation of card image

--- components/course-card.tsx
+++ components/course-card.tsx
@@ -28,7 +28,7 @@ const CourseCard = ({
     <Link href={`/courses/${id}`}>
       <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
         <div className="relative w-full aspect-video rounded-md overflow-hidden">
-          <Image fill className="object-cover" alt={title} src={imageUrl} />
+          <Image fill className="object-cover hover:scale-110 transition" alt={title} src={imageUrl} />
         </div>
         <div className="flex flex-col pt-2">
           <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition line-clamp-2">
